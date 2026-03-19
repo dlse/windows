eefe9eadbf984acd78f827bfb09e0f7a0fdbb1b1
dlse <30324819+dlse@users.noreply.github.com>
2026-03-19 13:43:11 -0400

    add files

---
+++ get-md5.ps1
@@ -0,0 +1,3 @@
+﻿get-childitem "directory_1" | foreach-object {get-filehash -algorithm md5 $_.name} | fl | out-file -force -filepath 'directory_2/md5.txt'
+
+get-filehash * | fl | out-file -force -filepath 'directory/checksum_sha256.txt
